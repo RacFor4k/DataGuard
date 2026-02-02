38612b28fd0c7c43da12068023f896b5e9a412be
RacFor4k <racfor4@gmail.com>
2026-02-02 16:28:18 +0300

    Преименовал папки для соответствия стандартам

---
+++ Server/Contracts/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using ProtoBuf;
+using ProtoBuf.Grpc;
+using ProtoBuf.Grpc.Configuration;
+using Server.Models.gRPC.Account;
+
+namespace Server.Services
+{
+    public class AccountService
+    {
+        [Service]
+        public interface IAccountServise
+        {
+            ValueTask<SignUpResponce> signUp(SignUpRequest request, CallContext context = default);
+            ValueTask<SignInResponce> signIn(SignInRequest request, CallContext context = default);
+
+            ValueTask<CreateCompanyResponce> createCompany(CreateCompanyRequest request, CallContext context = default);
+            ValueTask<LiquidateCompanyResponce> liquidateCompany(LiquidateCompanyRequest request, CallContext context = default);
+
+        }
+    }
+}
---
+++ Server/Models/gRPC/Account/SignIn.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+
+namespace Server.Models.gRPC.Account
+{
+    [ProtoContract]
+    public class SignInResponce
+    {
+        [ProtoMember(1)] public string Name { get; set; } = "";
+        [ProtoMember(2)] public string Surname { get; set; } = "";
+        [ProtoMember(3)] public string Email { get; set; } = "";
+        [ProtoMember(4)] public string EncryptedToken { get; set; } = "";
+        [ProtoMember(5)] public List<KeyValuePair<int, string>> Groups { get; set; } = new List<KeyValuePair<int, string>>();
+    }
+
+    [ProtoContract]
+    public class SignInRequest
+    {
+        [ProtoMember(1)] public string PublicKey { get; set; } = "";
+    }
+}
