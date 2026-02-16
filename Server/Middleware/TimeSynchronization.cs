43ee52c718b7d225fb1299f4f60f1b68bc2024c7
RacFor4k <racfor4@gmail.com>
2026-02-16 10:13:47 +0300

    реализован idempotency и timeSynchronization

---
+++ Server/Middleware/TimeSynchronization.cs
@@ -0,0 +1,35 @@
+﻿namespace Server.Middleware
+{
+    public class TimeSynchronization
+    {
+        private readonly RequestDelegate _next;
+
+        public TimeSynchronization(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Headers.TryGetValue("X-Request-Time", out var TimeValues))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid X-Request-Time time");
+                return;
+            }
+            if (!DateTime.TryParse(TimeValues.FirstOrDefault(), out var ClientTime))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid X-Request-Time time format");
+                return;
+            }
+            if (DateTime.UtcNow.Subtract(ClientTime).Hours < 1)
+            {
+                await _next(context);
+                return;
+            }
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsync("Client and server times differ by more than 1 hour");
+        }
+    }   
+}
