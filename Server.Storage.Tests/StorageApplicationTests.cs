c52bdbc832a60e22693526a8e06260746ae30c6f
RacFor4k <racfor4@gmail.com>
2026-06-14 12:19:24 +0300

    refactor(Server.Storage.Tests): удаление устаревших тестов
    - [StorageApplicationTests.cs]: удален тест WeatherForecast_WithoutAuthentication_IsNotSuccessful, так как он не соответствует текущей функциональности
    - [WeatherForecastControllerTests.cs]: удален файл с тестами для WeatherForecastController, так как он больше не используется

--- Server.Storage.Tests/StorageApplicationTests.cs
+++ Server.Storage.Tests/StorageApplicationTests.cs
@@ -10,14 +10,4 @@ public class StorageApplicationTests : IClassFixture<WebApplicationFactory<Progr
     {
         _factory = factory;
     }
-
-    [Fact]
-    public async Task WeatherForecast_WithoutAuthentication_IsNotSuccessful()
-    {
-        using var client = _factory.CreateClient();
-
-        var response = await client.GetAsync("/WeatherForecast");
-
-        Assert.False(response.IsSuccessStatusCode);
-    }
 }
